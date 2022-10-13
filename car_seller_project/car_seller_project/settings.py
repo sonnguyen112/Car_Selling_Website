6daebe5348415c56dfad9d9723f063fa2b90997f
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2022-10-13 23:46:26 +0700

    Added Car App and fetch data to home

--- car_seller_project/car_seller_project/settings.py
+++ car_seller_project/car_seller_project/settings.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "pages",
     "cars",
+    "ckeditor",
+    "django.contrib.humanize",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
